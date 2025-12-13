49bf5b28c33d523ba5b755148a534d86a65b4138
Axel-PC <axel.bergenfeldt@gmail.com>
2025-12-13 05:27:02 +0100

    AoC, done with 41

--- AoC/2025/code.py
+++ AoC/2025/code.py
@@ -1,4 +1,5 @@
-
+import random as rn
+from tqdm import tqdm
 
 def main30() :
     list = []
@@ -29,37 +30,162 @@ def main30() :
 
 
 def main31() :
+    solSize = 12
+    print_ = False
+    brute = False
     list = []
-    with open("AoC\\2025\\input3_test.txt", "r") as file :
+    with open("AoC\\2025\\input3.txt", "r") as file :
         for line in file :
             list.append(str(int(line)))
     sol = []
     for number in list :
-        numbers = []
-        lastInt = -1
-        lastIndex = -1
-        for j in range(12) :
-            index = 0
-            biggest = 0
-            for i, c in enumerate(number) :
-                n = int(c)
-                if n >= biggest :
+        l = len(number)
+        indexes = {}
+        lastIndexes = []
+        startIndex = 0
+        endIndex = l
+        for j in range(solSize) :
+            newInt = False
+            i = startIndex
+            biggest = -1
+            biggestIndex = -1
+            while not newInt :
+                n = int(number[i])
+                if biggest < n and (indexes.get(i, -1) == -1) :
                     biggest = n
-                    index = i
-                # print(biggest, "and index:", index)
-            numbers.append((biggest,index))
-            if index < lastIndex :
-                #?
-                print("hej")
-            lastInt = biggest
-            lastIndex = index
-            number = number[:index] + number[index+1:]
-        print(numbers)
+                    biggestIndex = i
+                i += 1
+                if i >= endIndex :
+                    if biggest != -1 :
+                        indexes[biggestIndex] = biggest
+                        lastIndexes.append(biggestIndex)
+                        newInt = True
+                        startIndex = biggestIndex
+                    else :
+                        endIndex = startIndex
+                        if len(lastIndexes) != 0 :
+                            lastIndex = lastIndexes[-1]
+                            if sum([1 for key in indexes if key > lastIndex]) == (l - lastIndex - 1) :
+                                i = lastIndexes.pop()
+                            else :
+                                i = lastIndex
+                        else :
+                            i = 0
+        num = ""
+        for k in range(l) :
+            num += str(indexes.get(k, ""))
+        num = int(num)
+        sol.append(num)
+    if brute :
+        altSol = []
+        for n in tqdm(list) :
+            allComb = []
+            for i1 in range(l-11) :
+                for i2 in range(i1+1,l-10) :
+                    for i3 in range(i2+1,l-9) :
+                        for i4 in range(i3+1,l-8) :
+                            for i5 in range(i4+1,l-7) :
+                                for i6 in range(i5+1,l-6) :
+                                    for i7 in range(i6+1,l-5) :
+                                        for i8 in range(i7+1,l-4) :
+                                            for i9 in range(i8+1,l-3) :
+                                                for i10 in range(i9+1,l-2) :
+                                                    for i11 in range(i10+1,l-1) :
+                                                        for i12 in range(i11+1,l) :
+                                                            allComb.append(n[i1] + n[i2] + n[i3] + n[i4] + n[i5] + n[i6] + n[i7] + n[i8] + n[i9] + n[i10] + n[i11] + n[i12])
+            allComb.sort()
+            altSol.append(int(allComb[-1]))
+        # print(altSol)
+    if print_ :
+        for i in range(len(sol)) :
+            print(i, ":", list[i], "\nMySol", sol[i])
+            if brute :
+                print("Brute", altSol[i])
+                if sol[i] == altSol[i] :
+                    print("Pass")
+                else :
+                    print("-----------------------------> Fail")
+        print(len(sol), len(list))
+    print(sum(sol))
 
-def contains(list, value) :
-    for n in list :
-        if n == value :
-            return True
-    return False
+def makeInput() :
+    with open("AoC\\2025\\data.txt", "w") as file:
+        for i in range(20) :
+            n = 0
+            while "0" in str(n) :
+                n = rn.randint(10000000000000000,99999999999999999)
+            file.write(str(n) + "\n")
 
-main31()
+
+
+def main40() :
+    size = 1
+    list = []
+    with open("AoC\\2025\\input4.txt", "r") as file :
+        ln = 0
+        for line in file :
+            list.append("".join([c for c in line if c != "\n"]))
+    print(list)
+    m = len(list)
+    n = len(list[0])
+    counter = {}
+    acc = 0
+    for i in range(m) :
+        for j in range(n) :
+            if list[i][j] == "@" :
+                for k in range(-size, size + 1) :
+                    for l in range(-size, size + 1) :
+                        if k != 0 or l != 0 :
+                            counter.setdefault(((i + k),(j + l)), 0)
+                            counter[((i + k),(j + l))] += 1
+    for i in range(m) :
+        for j in range(n) :
+            if counter.get((i,j), 0) < 4 and list[i][j] == "@" :
+                acc += 1
+    print(acc)
+
+
+
+def main41() :
+    size = 1
+    list = []
+    with open("AoC\\2025\\input4.txt", "r") as file :
+        ln = 0
+        for line in file :
+            list.append("".join([c for c in line if c != "\n"]))
+    print(list)
+    m = len(list)
+    n = len(list[0])
+    acc = 0
+    change = -1
+    while change != 0 :
+        counter = {}
+        change = 0
+        for i in range(m) :
+            for j in range(n) :
+                if list[i][j] == "@" :
+                    for k in range(-size, size + 1) :
+                        for l in range(-size, size + 1) :
+                            if k != 0 or l != 0 :
+                                counter.setdefault(((i + k),(j + l)), 0)
+                                counter[((i + k),(j + l))] += 1
+        for i in range(m) :
+            for j in range(n) :
+                if counter.get((i,j), 0) < 4 and list[i][j] == "@" :
+                    change += 1
+                    string = list[i] 
+                    list[i] = string[:j] + "." + string[j+1:]
+        # print(list)
+        acc += change
+    print(acc)
+
+
+
+def main41() :
+    list = []
+    with open("AoC\\2025\\input5_test.txt", "r") as file :
+        for line in file :
+            list.append("".join([c for c in line if c != "\n"]))
+    print(list)
+
+main41()
